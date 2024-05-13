41e3452444b3ea48cd7da162dfdf8e89cdfc0c8b
Margot Pasquali <margotpasquali1@gmail.com>
2024-05-13 19:20:19 +0200

    add DishView and modify WelcomeView

---
+++ TajMahal/TajMahal/Views/DishDetail.swift
@@ -0,0 +1,29 @@
+//
+//  CourseDetail.swift
+//  TajMahal
+//
+//  Created by Margot Pasquali on 13/05/2024.
+//
+
+import SwiftUI
+
+struct DishDetail: View {
+    let viewModel: ViewModel = ViewModel()
+    
+    var body: some View {
+        Text("Nom du plat")
+            .font(.title)
+            .multilineTextAlignment(.leading)
+        Image("Tikka Masala")
+            .resizable()
+            .aspectRatio(contentMode: .fill) // Garde les proportions de l'image tout en la remplissant
+            .frame(width: 300, height: 400) // Définit la taille de l'image
+            .clipShape(RoundedRectangle(cornerRadius: 10)) // Applique des bords arrondis avec un rayon de 10
+
+        Spacer()
+    }
+}
+
+#Preview {
+    DishDetail()
+}
--- TajMahal/TajMahal/Views/WelcomeView.swift
+++ TajMahal/TajMahal/Views/WelcomeView.swift
@@ -12,11 +12,43 @@ struct WelcomeView: View {
     var body: some View {
         NavigationStack {
             Image("TajMahal")
+            HStack {
+                VStack (alignment: .leading){
+                        Text("Restaurant Indien")
+                            .font(.title2)
+                        
+                        Text("Taj Mahal")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+            }
+            VStack (alignment: .leading) {
+                HStack {
+                    Text("Mardi")
+                    .font(.subheadline)
+                    Text("11h30 - 14h30 . 18h30 - 22h00")
+                    .font(.subheadline)
+                }
+                
+                HStack {
+                    Text("Type de service")
+                    .font(.subheadline)
+                    Text("A emporter")
+                    .font(.subheadline)
+                }
+                
+                Text("12 avenue de la Brique - 75010 Paris")
+                    .font(.subheadline)
+                Text("www.tajmahal.fr")
+                    .font(.subheadline)
+                Text("06 12 34 56 78")
+                    .font(.subheadline)
+            }
+            
             Spacer()
             NavigationLink {
                 MenuView()
             } label : {
-                Text("Menu")
+                Text("Accéder au menu")
             }
         }
     }
